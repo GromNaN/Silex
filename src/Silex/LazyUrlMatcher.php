486e536c7a7661e29484a425257bb7b7b5e43437
Jerome TAMARELLE <jtamarelle@groupe-exp.com>
2014-04-15 11:27:49 +0200

    Implement the RequestMatcherInterface

--- src/Silex/LazyUrlMatcher.php
+++ src/Silex/LazyUrlMatcher.php
@@ -13,13 +13,15 @@ namespace Silex;
 
 use Symfony\Component\Routing\RequestContext as SymfonyRequestContext;
 use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use Symfony\Component\Routing\Matcher\RequestMatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Implements a lazy UrlMatcher.
  *
  * @author Igor Wiedler <igor@wiedler.ch>
  */
-class LazyUrlMatcher implements UrlMatcherInterface
+class LazyUrlMatcher implements UrlMatcherInterface, RequestMatcherInterface
 {
     private $factory;
 
@@ -36,8 +38,8 @@ class LazyUrlMatcher implements UrlMatcherInterface
     public function getUrlMatcher()
     {
         $urlMatcher = call_user_func($this->factory);
-        if (!$urlMatcher instanceof UrlMatcherInterface) {
-            throw new \LogicException("Factory supplied to LazyUrlMatcher must return implementation of UrlMatcherInterface.");
+        if (!$urlMatcher instanceof UrlMatcherInterface && !$urlMatcher instanceof RequestMatcherInterface) {
+            throw new \LogicException("Factory supplied to LazyUrlMatcher must return implementation of UrlMatcherInterface or RequestMatcherInterface.");
         }
 
         return $urlMatcher;
@@ -49,6 +51,21 @@ class LazyUrlMatcher implements UrlMatcherInterface
     public function match($pathinfo)
     {
         return $this->getUrlMatcher()->match($pathinfo);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function matchRequest(Request $request)
+    {
+        $matcher = $this->getUrlMatcher();
+
+        // matching a request is more powerful than matching a URL path + context, so try that first
+        if ($matcher instanceof RequestMatcherInterface) {
+            return $matcher->matchRequest($request);
+        }
+
+        return $matcher->match($request->getPathInfo());
     }
 
     /**
